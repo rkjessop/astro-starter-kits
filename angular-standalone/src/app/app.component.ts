efb6e17b679bc5d10129ef858eb08b460a0e5ec4
rkjessop <rkjessop@gmail.com>
2024-04-18 13:38:47 -0400

    OMW-0: Initial revision

--- angular-standalone/src/app/app.component.ts
+++ angular-standalone/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AstroComponentsModule } from '@astrouxds/angular';
 
@@ -7,8 +7,30 @@ import { AstroComponentsModule } from '@astrouxds/angular';
   standalone: true,
   imports: [RouterOutlet, AstroComponentsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.scss',
+  //schemas: [NO_ERRORS_SCHEMA /*CUSTOM_ELEMENTS_SCHEMA*/]
+  // imports: [BrowserModule, FormsModule, AstroComponentsModule],  
 })
 export class AppComponent {
-  title = 'angular-standalone';
+  title = 'angular-standalone'; // TODO: unnecessary?  It is in package.json and elsewhere.
+  systemClassification = "UNCLASSIFIED";
+  applicationAbbreviation = "NG OMW";
+  applicationName = "NG Orbital Mechanics Workbench";
+  version = "0.1";
+
+  menuSelect(e: CustomEvent) {
+    console.error('##### >menuSelect:e = ', e);
+    const { detail } = e;
+    if (detail.value === 'notImplemented') {
+      console.error('##### b');
+      // addToast('This feature has not been implemented', false, 3000);
+      return;
+    }
+    if (detail.value === 'themeToggle') {
+      console.error('##### a');
+      // setLightTheme(!lightTheme);
+      // document.body.classList.toggle('light-theme');
+      return;
+    }
+  }  
 }
