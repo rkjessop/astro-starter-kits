90695f03c8c2190f1dd404526fd7902e05f95417
rkjessop <rkjessop@gmail.com>
2024-04-22 17:05:41 -0400

    OMW-4: Integrate vertical scrollbar, fix header row, make rowData an observable
    WIP.

--- angular-standalone/src/launch-list/launch-list.component.ts
+++ angular-standalone/src/launch-list/launch-list.component.ts
@@ -1,83 +1,124 @@
 import { AngularLibComponent, AstroComponentsModule, RuxContainer } from '@astrouxds/angular';
-import { Component } from '@angular/core';
-import { AgGridAngular } from 'ag-grid-angular';
+import { Component, OnInit } from '@angular/core';
+import { AgGridAngular, AgGridModule } from 'ag-grid-angular';
 import { HttpClient } from '@angular/common/http';
-import { ColDef, GridReadyEvent } from 'ag-grid-community';
+import { ColDef, GridOptions, GridReadyEvent, SuppressKeyboardEventParams } from 'ag-grid-community';
 import { HttpClientModule } from '@angular/common/http';
 import { Store } from '@ngrx/store';
 import { set } from '../app/counter.actions';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-launch-list',
   standalone: true,
-  imports: [AstroComponentsModule, HttpClientModule, AgGridAngular],
+  imports: [AstroComponentsModule, HttpClientModule, AgGridAngular, CommonModule],
   templateUrl: './launch-list.component.html',
   styleUrl: './launch-list.component.scss'
 })
-export class LaunchListComponent {
+export class LaunchListComponent implements OnInit {
   severitySelection: any;
   categorySelection: any;
   sortDirection: any;
 
-  // Row Data: The data to be displayed.
-  rowData = [
-    { make: "Tesla", model: "Model Y", price: 64950},//, electric: true },
-    // { make: "Ford", model: "F-Series", price: 33850},//, electric: false },
-    // { make: "Toyota", model: "Corolla", price: 29600}//, electric: false },
-  ];
+  rowData = new BehaviorSubject<any []>([]);
+  // rowData$: this.rowData.asObservable();
 
   defaultColDef: ColDef = {
-    editable: false,
-    enableRowGroup: false,
+    // editable: false,
+    // enableRowGroup: false,
     // enablePivot: true,
     // enableValue: true,
-    filter: false,
-    flex: 1,
-    minWidth: 100,
-    maxWidth: 150,
+    // filter: false,
+    // flex: 1,
+    // minWidth: 100,
+    // maxWidth: 250,
+    // padding: var(--spacing-0, 0rem) var(--spacing-2, 0.5rem);    
   };
+
   colDefs: ColDef[] = [
     { 
-      headerName: 'xxx',
-      field: "athlete",
+      headerName: 'Date',
+      field: "date",
       width: 100
     },
     {
-      headerName: 'yyy',
+      headerName: 'Launch Window',
       field: "age",
-      width: 100 
+      width: 170
+      , wrapHeaderText: true
     },
-    // { 
-    //   headerName: 'zzz',
-    //   field: "price",
-    //   width: 100 
-    // },
-    // {
-    //   field: "electric",
-    //   width: 100 
-    // }
+    { 
+      headerName: 'Launch Site',
+      field: "athlete",
+      width: 210
+    },
+    {
+      headerName: 'Mission Description',
+      field: "sport",
+      width: 100,
+      flex: 1
+    }
   ];
+
+  gridOptions: GridOptions;
 
   constructor(
     private http: HttpClient
     , private store: Store<{ count: number }>
   ) {
-
+    this.gridOptions = {
+      domLayout: 'autoHeight',
+      defaultColDef: {
+        sortable: false,
+        resizable: false,
+        editable: false,
+        suppressMenu: true,
+        suppressKeyboardEvent: (params) => { 
+          return this.suppressEnter(params) || this.suppressNavigation(params);
+        },
+        filter: false,
+        floatingFilter: false,
+        filterParams: { buttons: ['clear'] }
+      },
+      rowHeight: 50,
+      paginationPageSize: 10,
+      headerHeight: 40,
+      animateRows: false,
+      pagination: false,
+      paginationAutoPageSize: false,
+    }
   }
 
-  onGridReady(params: GridReadyEvent<any>) {
-    console.log('##### >onGridReady');
+  ngOnInit() {
     this.http
       .get<
         any[]
       >("https://www.ag-grid.com/example-assets/olympic-winners.json")
       .subscribe((data) => {
-        console.error('############ data = ', data);
-        console.error('############ data.length = ', data.length);
-        this.rowData = data;
+        this.rowData.next(data);
         this.store.dispatch(set({count: data.length}));
       });
+  }
+
+  suppressEnter(params: SuppressKeyboardEventParams) {
+    var KEY_ENTER = "Enter";
+  
+    var event = params.event;
+    var key = event.key;
+    var suppress = key === KEY_ENTER;
+  
+    return suppress;
   }  
+
+  suppressNavigation(params: SuppressKeyboardEventParams<any, any>): boolean {
+    return true;
+  }
+  
+  onGridReady(params: GridReadyEvent<any>) {
+    params.api.sizeColumnsToFit();
+    }  
 
   handleClick(e: any) {
     console.error('##### >handleClick');
